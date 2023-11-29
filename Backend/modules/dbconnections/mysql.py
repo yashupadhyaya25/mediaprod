73f9d5d53a3d4f5a60a9ab2acbaee90812ae1db2
yash-upadhyaya <yash.u@pipluspi.com>
2023-11-29 15:54:42 +0530

    Update mysql.py

--- Backend/modules/dbconnections/mysql.py
+++ Backend/modules/dbconnections/mysql.py
@@ -3,5 +3,5 @@ import sqlalchemy
 class mysql() :    
     def connection(connection_string):
         conn_str = connection_string
-        conn = sqlalchemy.create_engine(conn_str,pool_szie = 50,max_overflow =0)
+        conn = sqlalchemy.create_engine(conn_str)
         return conn
