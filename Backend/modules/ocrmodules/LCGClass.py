20daf7064762fc05bf5871474bd8bc85c01ef4a2
yash-upadhyaya <yash.u@pipluspi.com>
2023-12-07 17:14:41 +0530

    restaurant ocr added

--- Backend/modules/ocrmodules/LCGClass.py
+++ Backend/modules/ocrmodules/LCGClass.py
@@ -86,10 +86,18 @@ class LCGClass():
                     previous_page_invoice_number = invoice_number
                     file_df['UNIQUE_IDENTIFICATION_NUMBER'] = invoice_number  
                 else :
-                    file_df['UNIQUE_IDENTIFICATION_NUMBER'] = previous_page_invoice_number  
-                
+                    file_df['UNIQUE_IDENTIFICATION_NUMBER'] = previous_page_invoice_number 
+                    
+                restaurant_name = LCGClass.getRestaurantName(page)
+                if str(restaurant_name) != 'nan': 
+                    previous_restaurant_name = restaurant_name
+                    file_df['RESTAURANT_NAME'] = restaurant_name  
+                else :
+                    file_df['RESTAURANT_NAME'] = previous_restaurant_name
+                # print(restaurant_name)
                 page_no += 1
                 final_df = pd.concat([final_df,file_df],axis=0)
+                
             
             final_df.drop_duplicates(inplace=True)
             
@@ -176,12 +184,19 @@ class LCGClass():
                     main_df['UNIQUE_IDENTIFICATION_NUMBER'] = invoice_number  
                 else :
                     main_df['UNIQUE_IDENTIFICATION_NUMBER'] = previous_page_invoice_number  
+                
+                restaurant_name = LCGClass.getRestaurantName(page)
+                if str(restaurant_name) != 'nan': 
+                    previous_restaurant_name = restaurant_name
+                    main_df['RESTAURANT_NAME'] = restaurant_name  
+                else :
+                    main_df['RESTAURANT_NAME'] = previous_restaurant_name
 
                 final_df = pd.concat([final_df,main_df],axis=0,ignore_index=True)              
                 page_no += 1 
                                 
             fp.close()
-            final_df = final_df.loc[:,['Sous-total (HT)','Total (HT)','EU-Norm-FR-5.5 (5.5000%)','Taxe','Sous-total (TTC)','Total (TTC)','Invoice Date','UNIQUE_IDENTIFICATION_NUMBER']]
+            final_df = final_df.loc[:,['Sous-total (HT)','Total (HT)','EU-Norm-FR-5.5 (5.5000%)','Taxe','Sous-total (TTC)','Total (TTC)','Invoice Date','UNIQUE_IDENTIFICATION_NUMBER','RESTAURANT_NAME']]
             final_df = final_df.rename(columns={
                 'Total (TTC)' : 'TOTAL_TTC',
                 'Total (HT)' : 'TOTAL_HT',
@@ -233,7 +248,7 @@ class LCGClass():
         except :
             return np.NaN
 
-    def getInvoiceNumber(file_path) :
+    def getInvoiceNumber(page) :
         try :
             rsrcmgr = PDFResourceManager()
             laparams = LAParams(word_margin=0.1)
@@ -242,7 +257,7 @@ class LCGClass():
             final_df = pd.DataFrame()
             X = 0
             Y = 0
-            interpreter.process_page(file_path)
+            interpreter.process_page(page)
             layout = device.get_result()
             for lobj in layout:
                 if isinstance(lobj, LTTextBox):
@@ -272,6 +287,54 @@ class LCGClass():
             return invoice_number
         except :
             return np.NaN
+    
+    def getRestaurantName(page) :
+        try :
+            rsrcmgr = PDFResourceManager()
+            laparams = LAParams(word_margin=0.1)
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            final_df = pd.DataFrame()
+            X = 0
+            Y = 0
+            interpreter.process_page(page)
+            layout = device.get_result()
+            for lobj in layout:
+                if isinstance(lobj, LTTextBox):
+                    df = pd.DataFrame(columns=['Y0','Y1','X0','X1','DETAIL'])
+                    text = lobj.get_text()     
+                    coordinate = str(lobj.bbox)[1:-1].split(",")
+                    df['Y0'] = [coordinate[0]]
+                    df['Y1'] = [coordinate[1]]
+                    df['X0'] = [coordinate[2]]
+                    df['X1'] = [coordinate[3]]
+                    df['DETAIL'] = text
+                    if 'Vendu à :' in text:
+                        X = float(coordinate[1])
+                        Z = float(coordinate[2])
+                    if 'Mode de paiement' in text:
+                        Y = float(coordinate[1])
+                    final_df = pd.concat([final_df,df],axis=0,ignore_index=True)
+            final_df = final_df[final_df['Y1'].astype(float)<float(X)]
+            final_df = final_df[final_df['Y1'].astype(float)>float(Y)]
+            final_df['X1'] = final_df['X1'].astype(float)
+            final_df['X0'] = final_df['X0'].astype(float)
+            final_df = final_df.sort_values(by=['X1','X0'],ignore_index=True)
+            final_df = final_df['DETAIL'].head(1)
+            
+            restaurant_name = final_df.values[0].split('\n')
+            restaurant_name = [name.lower() for name in restaurant_name]
+            if '29 rue de trévise' in restaurant_name or '29 rue trévise' in restaurant_name :
+                return 'BON BOUQUET'
+            elif '7 rue keller' in restaurant_name :
+                return 'KAFKAF'
+            elif '96 bd de sébastopol' in restaurant_name or '96 boulevard de sebastopol' in restaurant_name :
+                return 'SANTA LYNA'
+            else :
+                return final_df.values[0].split('\n')[1]
+        except :
+            return np.NaN
+                    
     
 if __name__ == '__main__':
     pass
--- Backend/modules/ocrmodules/ReynaudClass.py
+++ Backend/modules/ocrmodules/ReynaudClass.py
@@ -21,6 +21,8 @@ class ReynaudClass():
         Y = 0
         try :
             for page in pages:
+                page_df = pd.DataFrame()
+                co_ordinated_dic = {}
                 interpreter.process_page(page)
                 layout = device.get_result()
                 for lobj in layout:
@@ -37,51 +39,50 @@ class ReynaudClass():
                             X = float(coordinate[1])
                         if 'Total postes' in text:
                             Y = float(coordinate[1])
-                        final_df = pd.concat([final_df,df],axis=0,ignore_index=True)
-                break
+                        page_df = pd.concat([page_df,df],axis=0,ignore_index=True)
+                   
+                if page_df.size > 235 :
+                    page_df = page_df[page_df['Y1'].astype(float)<X]
+                    page_df = page_df[page_df['Y1'].astype(float)>Y]
+                    page_df['KEY'] = page_df['Y1'] + '_' + page_df['X1']
 
-            co_ordinated_dic = {}
-            missing_coordianted_dic = {}
-            
-            final_df = final_df[final_df['Y1'].astype(float)<X]
-            final_df = final_df[final_df['Y1'].astype(float)>Y]
-            final_df['KEY'] = final_df['Y1'] + '_' + final_df['X1']
-            # print(final_df)
-            key_df = final_df['KEY'].unique()
-            for key in key_df:
-                if final_df['KEY'].value_counts()[key] > 1 :
-                    co_ordinated_dic[key] = final_df['KEY'].value_counts()[key]
+                    key_df = page_df['KEY'].unique()
+                    for key in key_df:
+                        if page_df['KEY'].value_counts()[key] > 1 :
+                            co_ordinated_dic[key] = page_df['KEY'].value_counts()[key]
                     
-            description_df = pd.DataFrame()   
+                    description_df = pd.DataFrame()   
             
-            for filter_key in co_ordinated_dic.keys():
-                temp_df = pd.DataFrame()
-                temp_df = final_df.loc[(final_df.KEY == filter_key)]
-                description_df = pd.concat([description_df,temp_df],axis=0)
-            
-            no_of_rows = int(len(description_df))
-            no_of_table_rows = int(len(description_df)//7)
+                    for filter_key in co_ordinated_dic.keys():
+                        temp_df = pd.DataFrame()
+                        temp_df = page_df.loc[(page_df.KEY == filter_key)]
+                        description_df = pd.concat([description_df,temp_df],axis=0)
+                    
+                    no_of_rows = int(len(description_df))
+                    no_of_table_rows = int(len(description_df)//7)
             
     
-            description_df = description_df.sort_values(by=['X1','Y0'],ignore_index=False)
-            final_df_details = description_df['DETAIL']
-            file_df = pd.DataFrame()
-            for item in np.array_split(final_df_details, (no_of_table_rows)):
-                temp_df = pd.DataFrame(item)
-                temp_df.reset_index(drop=True,inplace=True)
-                temp_df = temp_df.transpose()
-                file_df = pd.concat([file_df,temp_df],axis=0)
+                    description_df = description_df.sort_values(by=['X1','Y0'],ignore_index=False)
+                    final_df_details = description_df['DETAIL']
+                    file_df = pd.DataFrame()
+                    for item in np.array_split(final_df_details, (no_of_table_rows)):
+                        temp_df = pd.DataFrame(item)
+                        temp_df.reset_index(drop=True,inplace=True)
+                        temp_df = temp_df.transpose()
+                        file_df = pd.concat([file_df,temp_df],axis=0)
                             
-            file_df.rename(columns={0:'Article',1:'Colis',2:'Quantité',3:'Poids (KG)',4:'Prix unitaire',5:'Total HT',6:'Désignation'},inplace=True)
+                    file_df.rename(columns={0:'Article',1:'Colis',2:'Quantité',3:'Poids (KG)',4:'Prix unitaire',5:'Total HT',6:'Désignation'},inplace=True)
+                    invoice_date = ReynaudClass.getInvoiceDate(page)
+                    file_df['Invoice Date'] = invoice_date
+                    invoice_number = ReynaudClass.getInvoiceNumber(page)
+                    file_df['UNIQUE_IDENTIFICATION_NUMBER'] = invoice_number
+                    restaurant_name = ReynaudClass.getRestaurantName(page)
+                    file_df['RESTAURANT_NAME'] = restaurant_name
+                    final_df = pd.concat([file_df,final_df],axis=0)
             fp.close()
             
-            invoice_date = ReynaudClass.getInvoiceDate(file_path)
-            file_df['Invoice Date'] = invoice_date
-            invoice_number = ReynaudClass.getInvoiceNumber(file_path)
-            file_df['UNIQUE_IDENTIFICATION_NUMBER'] = invoice_number
-            
-            file_df = file_df.loc[:,['Article','Désignation','Colis','Quantité','Poids (KG)','Prix unitaire','Total HT','Invoice Date','UNIQUE_IDENTIFICATION_NUMBER']]
-            file_df = file_df.rename(columns={
+            final_df = final_df.loc[:,['Article','Désignation','Colis','Quantité','Poids (KG)','Prix unitaire','Total HT','Invoice Date','UNIQUE_IDENTIFICATION_NUMBER','RESTAURANT_NAME']]
+            final_df = final_df.rename(columns={
                 'Article' : 'CODE',
                 'Désignation' : 'DESCRIPTION',
                 'Quantité' : 'QUANTITY',
@@ -89,8 +90,9 @@ class ReynaudClass():
                 'Total HT' : 'TOTAL_AMOUNT',
                 'Invoice Date' : 'INVOICE_DATE',
             })
-            file_df = file_df.astype(str)
-            return file_df
+            final_df = final_df.astype(str)
+            print(final_df)
+            return final_df
         except :
             fp.close()
             return final_df
@@ -108,6 +110,7 @@ class ReynaudClass():
         Z = 0
         try :
             for page in pages:
+                page_df = pd.DataFrame()
                 interpreter.process_page(page)
                 layout = device.get_result()
                 for lobj in layout:
@@ -125,81 +128,81 @@ class ReynaudClass():
                         if 'A PAYER (EUR)' in text:
                             Y = float(coordinate[1])
                             Z = float(coordinate[0])
-                        final_df = pd.concat([final_df,df],axis=0,ignore_index=True)
+                        page_df = pd.concat([page_df,df],axis=0,ignore_index=True)
 
-            final_df = final_df[final_df['Y1'].astype(float)<=float(X)]
-            final_df = final_df[final_df['Y1'].astype(float)>=float(Y)-10]
-            final_df = final_df.sort_values(by=['X1','Y0'],ignore_index=True)
-            final_df = pd.DataFrame(final_df['DETAIL'])
-
-    
-
-            main_df = pd.DataFrame()
-            for item in np.array_split(final_df, final_df.size//2):
-                test_df = pd.DataFrame()
-                temp_df = pd.DataFrame(item)
-                total_data = []
-                temp_df.reset_index(drop=True,inplace=True)
-                
-                for inx,data in temp_df.itertuples() :
-                    total_data.append(data)
-                test_df[total_data[0]] = [str(total_data[1]).strip()]
-                main_df = pd.concat([main_df,test_df],axis=1)
-            columns = main_df.columns
+                if page_df.size > 80 :
+                    page_df = page_df[page_df['Y1'].astype(float)<=float(X)]
+                    page_df = page_df[page_df['Y1'].astype(float)>=float(Y)-10]
+                    page_df = page_df.sort_values(by=['X1','Y0'],ignore_index=True)
+                    page_df = pd.DataFrame(page_df['DETAIL'])
+                    main_df = pd.DataFrame()
             
-            if 'TVA à 20,00% :' in columns :
-                main_df.drop(columns=['TVA à 20,00% :'],inplace=True)
-            if 'TVA à 5,50 % :' in columns :
-                main_df.drop(columns=['TVA à 5,50 % :'],inplace=True)     
-            if 'TVA à 5,50 % :\nTVA à 20,00% :' in columns :
-                main_df.drop(columns=['TVA à 5,50 % :\nTVA à 20,00% :'],inplace=True)
-            if 'BASE H.T. (1)\n BASE H.T. (2)' in columns :
-                main_df = main_df.rename(columns={'BASE H.T. (1)\n BASE H.T. (2)':'BASE H.T - (1)'})
-                main_df['BASE H.T - (1)'] = main_df['BASE H.T - (1)'].str.replace(',','.').astype(float)
-            if 'BASE H.T. :' in columns :
-                main_df['BASE H.T. :'] = main_df['BASE H.T. :'].str.replace(',','.').astype(float)
-        
-            if 'BASE H.T. :\n BASE H.T. :' in columns :
-                main_df[['BASE H.T 1','BASE H.T 2']] = main_df['BASE H.T. :\n BASE H.T. :'].str.split('\n',expand=True)
-                main_df['BASE H.T. :'] = main_df['BASE H.T 1'].str.replace(',','.').astype(float) + main_df['BASE H.T 2'].str.replace(',','.').astype(float)
-                main_df.drop(columns=['BASE H.T. :\n BASE H.T. :','BASE H.T 1','BASE H.T 2'],inplace=True)
+                    for item in np.array_split(page_df, page_df.size//2):
+                        test_df = pd.DataFrame()
+                        temp_df = pd.DataFrame(item)
+                        total_data = []
+                        temp_df.reset_index(drop=True,inplace=True)
+                        
+                        for inx,data in temp_df.itertuples() :
+                            total_data.append(data)
+                        test_df[total_data[0]] = [str(total_data[1]).strip()]
+                        main_df = pd.concat([main_df,test_df],axis=1)
+                    columns = main_df.columns
+                    
+                    if 'TVA à 20,00% :' in columns :
+                        main_df.drop(columns=['TVA à 20,00% :'],inplace=True)
+                    if 'TVA à 5,50 % :' in columns :
+                        main_df.drop(columns=['TVA à 5,50 % :'],inplace=True)     
+                    if 'TVA à 5,50 % :\nTVA à 20,00% :' in columns :
+                        main_df.drop(columns=['TVA à 5,50 % :\nTVA à 20,00% :'],inplace=True)
+                    if 'BASE H.T. (1)\n BASE H.T. (2)' in columns :
+                        main_df = main_df.rename(columns={'BASE H.T. (1)\n BASE H.T. (2)':'BASE H.T - (1)'})
+                        main_df['BASE H.T - (1)'] = main_df['BASE H.T - (1)'].str.replace(',','.').astype(float)
+                    if 'BASE H.T. :' in columns :
+                        main_df['BASE H.T. :'] = main_df['BASE H.T. :'].str.replace(',','.').astype(float)
+                
+                    if 'BASE H.T. :\n BASE H.T. :' in columns :
+                        main_df[['BASE H.T 1','BASE H.T 2']] = main_df['BASE H.T. :\n BASE H.T. :'].str.split('\n',expand=True)
+                        main_df['BASE H.T. :'] = main_df['BASE H.T 1'].str.replace(',','.').astype(float) + main_df['BASE H.T 2'].str.replace(',','.').astype(float)
+                        main_df.drop(columns=['BASE H.T. :\n BASE H.T. :','BASE H.T 1','BASE H.T 2'],inplace=True)
 
-            main_df['BASE H.T'] = main_df['BASE H.T. :'] + main_df['BASE H.T - (1)']
-            main_df.drop(columns=['BASE H.T. :','BASE H.T - (1)'],inplace=True)
+                    main_df['BASE H.T'] = main_df['BASE H.T. :'] + main_df['BASE H.T - (1)']
+                    main_df.drop(columns=['BASE H.T. :','BASE H.T - (1)'],inplace=True)
+                    
+                    invoice_date = ReynaudClass.getInvoiceDate(page)
+                    main_df['Invoice Date'] = invoice_date
+                    invoice_number = ReynaudClass.getInvoiceNumber(page)
+                    main_df['UNIQUE_IDENTIFICATION_NUMBER'] = invoice_number
+                    restaurant_name = ReynaudClass.getRestaurantName(page)
+                    main_df['RESTAURANT_NAME'] = restaurant_name
+                    final_df = pd.concat([final_df,main_df],axis=0)
             
             fp.close()
-            invoice_date = ReynaudClass.getInvoiceDate(file_path)
-            main_df['Invoice Date'] = invoice_date
             
-            invoice_number = ReynaudClass.getInvoiceNumber(file_path)
-            main_df['UNIQUE_IDENTIFICATION_NUMBER'] = invoice_number
-            
-            main_df = main_df.loc[:,['BASE H.T','TOTAL TVA','A PAYER (EUR)','Invoice Date','UNIQUE_IDENTIFICATION_NUMBER']]
-            main_df = main_df.rename(columns={
+            final_df = final_df.loc[:,['BASE H.T','TOTAL TVA','A PAYER (EUR)','Invoice Date','UNIQUE_IDENTIFICATION_NUMBER','RESTAURANT_NAME']]
+            final_df = final_df.rename(columns={
                 'A PAYER (EUR)' : 'TOTAL_TTC',
                 'TOTAL TVA' : 'TOTAL_TVA',
                 'BASE H.T' : 'TOTAL_HT',
                 'Invoice Date' : 'INVOICE_DATE'
             })
-            # print(main_df)
-            main_df = main_df.astype(str)
-            return main_df
+            final_df = final_df.astype(str)
+            return final_df
         except :
             fp.close()
             return final_df
 
-    def getInvoiceDate(file_path):
-        fp = open(file_path, 'rb')
-        rsrcmgr = PDFResourceManager()
-        laparams = LAParams(word_margin=0.1)
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        pages = PDFPage.get_pages(fp)
-        final_df = pd.DataFrame()
-        X = 0
-        Y = 0
-        Z = 0
-        for page in pages:
+    def getInvoiceDate(page):
+        try :
+            rsrcmgr = PDFResourceManager()
+            laparams = LAParams(word_margin=0.1)
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            pages = PDFPage.get_pages(page)
+            final_df = pd.DataFrame()
+            X = 0
+            Y = 0
+            Z = 0
             interpreter.process_page(page)
             layout = device.get_result()
             for lobj in layout:
@@ -219,32 +222,31 @@ class ReynaudClass():
                         Z = float(coordinate[0])
                     final_df = pd.concat([final_df,df],axis=0,ignore_index=True)
 
-        final_df = final_df[final_df['Y1'].astype(float)<=float(X)]
-        final_df = final_df[final_df['Y1'].astype(float)>=float(Y)]
-        final_df = final_df.sort_values(by=['X1','Y0'],ignore_index=True)
-        final_df = pd.DataFrame(final_df['DETAIL'])
-        
-        date_set = []
-        for data in final_df.itertuples() :
-            if 'Date Facture' in data[1] :
-                date_of_invoice = data[1].split('\n')[0].split(':')[1].strip()
-                date_set.append(date_of_invoice)
-        
-        fp.close()      
-        return list(set(date_set))
-    
-    def getInvoiceNumber(file_path) :
-        fp = open(file_path, 'rb')
-        rsrcmgr = PDFResourceManager()
-        laparams = LAParams(word_margin=0.1)
-        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        pages = PDFPage.get_pages(fp)
-        final_df = pd.DataFrame()
-        X = 0
-        Y = 0
-        Z = 0
-        for page in pages:
+            final_df = final_df[final_df['Y1'].astype(float)<=float(X)]
+            final_df = final_df[final_df['Y1'].astype(float)>=float(Y)]
+            final_df = final_df.sort_values(by=['X1','Y0'],ignore_index=True)
+            final_df = pd.DataFrame(final_df['DETAIL'])
+            
+            date_set = []
+            for data in final_df.itertuples() :
+                if 'Date Facture' in data[1] :
+                    date_of_invoice = data[1].split('\n')[0].split(':')[1].strip()
+                    date_set.append(date_of_invoice)   
+            return list(set(date_set))
+        except :
+            return np.NaN
+
+    def getInvoiceNumber(page) :
+        try :
+            rsrcmgr = PDFResourceManager()
+            laparams = LAParams(word_margin=0.1)
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            pages = PDFPage.get_pages(page)
+            final_df = pd.DataFrame()
+            X = 0
+            Y = 0
+            Z = 0
             interpreter.process_page(page)
             layout = device.get_result()
             for lobj in layout:
@@ -263,13 +265,62 @@ class ReynaudClass():
                         Y = float(coordinate[1])
                         Z = float(coordinate[0])
                     final_df = pd.concat([final_df,df],axis=0,ignore_index=True)
-        final_df = final_df[final_df['Y1'].astype(float)<=float(X)]
-        final_df = final_df[final_df['Y1'].astype(float)>=float(Y)]
-        final_df = pd.DataFrame(final_df['DETAIL'].str.replace('FACTURE N°',''))
-        final_df['DETAIL'] = final_df['DETAIL'].str.strip()
-        final_df.drop_duplicates(inplace=True)
-        fp.close()  
-        return (final_df.values)[0][0]
+            final_df = final_df[final_df['Y1'].astype(float)<=float(X)]
+            final_df = final_df[final_df['Y1'].astype(float)>=float(Y)]
+            final_df = pd.DataFrame(final_df['DETAIL'].str.replace('FACTURE N°',''))
+            final_df['DETAIL'] = final_df['DETAIL'].str.strip()
+            final_df.drop_duplicates(inplace=True)
+            return (final_df.values)[0][0]
+        except :
+            return np.NaN
+
+    def getRestaurantName(page) :
+        try :
+            rsrcmgr = PDFResourceManager()
+            laparams = LAParams(word_margin=0.1)
+            device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            final_df = pd.DataFrame()
+            X = 0
+            Y = 0
+            interpreter.process_page(page)
+            layout = device.get_result()
+            for lobj in layout:
+                if isinstance(lobj, LTTextBox):
+                    df = pd.DataFrame(columns=['Y0','Y1','X0','X1','DETAIL'])
+                    text = lobj.get_text()     
+                    coordinate = str(lobj.bbox)[1:-1].split(",")
+                    df['Y0'] = [coordinate[0]]
+                    df['Y1'] = [coordinate[1]]
+                    df['X0'] = [coordinate[2]]
+                    df['X1'] = [coordinate[3]]
+                    df['DETAIL'] = text
+                    if 'INFORMATIONS' in text:
+                        X = float(coordinate[1])
+                        Z = float(coordinate[2])
+                    if 'Article' in text:
+                        Y = float(coordinate[1])
+                    final_df = pd.concat([final_df,df],axis=0,ignore_index=True)
+            
+            final_df = final_df[final_df['Y1'].astype(float)<float(X)]
+            final_df = final_df[final_df['Y1'].astype(float)>float(Y)]
+            final_df = final_df.sort_values(by=['X1','X0'],ascending=False,ignore_index=True)
+            final_df = final_df['DETAIL'].head(1)
+
+            restaurant_name = final_df.values[0].split('\n')
+            restaurant_name = [name.lower() for name in restaurant_name]
+            print(restaurant_name)
+            if '29 rue de trévise' in restaurant_name or '29 rue trévise' in restaurant_name or '29 rue de trevise' in restaurant_name:
+                return 'BON BOUQUET'
+            elif '7 rue keller' in restaurant_name :
+                return 'KAFKAF'
+            elif '96 bd de sébastopol' in restaurant_name or '96 boulevard de sebastopol' in restaurant_name :
+                return 'SANTA LYNA'
+            else :
+                return final_df.values[0].split('\n')[0].replace(':','').upper().strip()
+        except :
+            return np.NaN
+
         
 if __name__ == '__main__':
     pass
